be49b0e240f9438ac369f8215d2caa7689485094
tagore404 <tagorech11@gmail.com>
2025-06-26 02:10:36 -0700

    Update OmsCustomerServiceApplicationTests.java

--- src/test/java/com/oms/customer/OmsCustomerServiceApplicationTests.java
+++ src/test/java/com/oms/customer/OmsCustomerServiceApplicationTests.java
@@ -1,16 +1,14 @@
 package com.oms.customer;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class OmsCustomerServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
